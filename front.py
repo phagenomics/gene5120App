1d666e5f0783210fed5169e9987dada3183dc5e7
J. Cesar Ignacio Espinoza <53498016+phagenomics@users.noreply.github.com>
2024-09-29 17:21:48 -0700

    added the html page for the new tool
    Added a new page on front.py

--- front.py
+++ front.py
@@ -21,5 +21,18 @@ def reverse_complement_tool():
 
     return render_template('reverse_complement.html', result=result, sequence=sequence)
 
+# Tool page for Reverse Complement
+@app.route('/tools/gc_content', methods=['GET', 'POST'])
+def gc_content_tool():
+    result = None
+    sequence = ""
+
+    if request.method == 'POST':
+        sequence = request.form.get('sequence', '').upper()
+        if sequence:
+            result = reverse_comp(sequence)
+
+    return render_template('gc_content.html', result=result, sequence=sequence)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
