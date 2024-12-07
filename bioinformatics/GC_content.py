7f8046d6124fa7f30aacd50ec3c134eef8ec6c4a
cindyg23 <cgarcia24@kgi.edu>
2024-12-07 14:55:40 -0800

    Update GC_content.py
    added primer design code

--- bioinformatics/GC_content.py
+++ bioinformatics/GC_content.py
@@ -5,3 +5,17 @@ def gc_calc(seq):
     count_a = seq.count('A')
     count_t = seq.count('T')
     return (count_c + count_g) / (count_c + count_g + count_a + count_t)
+
+## Primer Design code
+def comp(seq):
+  chargaff = {'A':'T', 'T':'A', 'G':'C', 'C':'G'}
+  comp = ' '
+  for char in seq:
+    comp = comp + chargaff[char]
+  return comp[::-1]
+
+def primers(seq):
+  fwd = seq[:10:]
+  rev = seq[-10:]
+
+  return fwd, comp(rev)
