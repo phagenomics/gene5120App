4bf0c8e7ddda089af70245208814e62731ca90ea
J. Cesar Ignacio Espinoza <53498016+phagenomics@users.noreply.github.com>
2025-02-18 14:34:20 -0800

    typo from palindromes to palindrome

--- front.py
+++ front.py
@@ -122,8 +122,8 @@ def restriction_tool():
     return render_template('restriction.html', result=result, sequence1=sequence1, sequence2=sequence2)
 
 # Tool page for Palindromes
-@app.route('/tools/palindromes', methods=['GET', 'POST'])
-def palindromes_tool():
+@app.route('/tools/palindrome', methods=['GET', 'POST'])
+def palindrome_tool():
     result = None
     sequence1 = ""
     sequence2 = ""
@@ -135,7 +135,7 @@ def palindromes_tool():
         if sequence1 and sequence2:
             result = hamming(sequence1, sequence2)
 
-    return render_template('palindromes.html', result=result, sequence1=sequence1, sequence2=sequence2)
+    return render_template('palindrome.html', result=result, sequence1=sequence1, sequence2=sequence2)
 
 
 
