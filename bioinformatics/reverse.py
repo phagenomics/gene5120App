5c85bdda3e09d97c3f7564ff59270cfb0c857684
J. Cesar Ignacio Espinoza <53498016+phagenomics@users.noreply.github.com>
2025-02-20 09:26:23 -0800

    Update reverse.py

--- bioinformatics/reverse.py
+++ bioinformatics/reverse.py
@@ -1,8 +1,1 @@
-def reverse_comp(sequence):
-    complement = ''
-    comp = {
-        'T': 'A', 'C': 'G', 'A': 'T', 'G': 'C'
-    }
-    for nucleotide in sequence:
-        complement += comp.get(nucleotide, 'N')  # Handle unexpected characters
-    return complement[::-1]
+##### removed reverse fucntion
