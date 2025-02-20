2fc2fbe826b21e27922c4a958b7427319f81063c
Janelle-Regina Deseo <168034957+janelledeseo@users.noreply.github.com>
2025-02-20 10:18:56 -0800

    Update reverse.py
    add 'strip' to remove trailing spaces

--- bioinformatics/reverse.py
+++ bioinformatics/reverse.py
@@ -1,6 +1,9 @@
 ### Janelle's Reverse Complement Tool
+## add 'strip' to remove trailing spaces
 
 def rev_comp(string):
   complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+  string = string.strip()
   reverse_complement = ''.join([complement[base] for base in string[::-1]])
   return reverse_complement
+
