a1053a6dc7c70e05ca45b912e9753886c286fc4b
Joe Uchytil <167820045+Juchytil@users.noreply.github.com>
2025-02-20 11:04:30 -0800

    Update reverse_palindrome_seq.py

--- bioinformatics/reverse_palindrome_seq.py
+++ bioinformatics/reverse_palindrome_seq.py
@@ -1,11 +1,9 @@
-test = 'TCAATGCATGCGGGTCTATATGCAT'
-
 def reverse_palindrome(seq,length):
-  compliment_lib = str.maketrans({'A':'T','T':'A','C':'G','G':'C'})
-  rev_comp_seq = seq.translate(compliment_lib)[::-1]
+  rev_seq = seq[::-1]
   n = length
   for i in range(len(seq)):
-    if seq[i:i+n] == rev_comp_seq[i:i+n]:
-      print(n+1,(seq[i:i+n]))
+    if seq[i:i+n] == rev_seq[i:i+n] and len(seq[i:i+n]) == n:
+      print(i+1,seq[i:i+n])
 
-reverse_palindrome(test, 4)
+
+
