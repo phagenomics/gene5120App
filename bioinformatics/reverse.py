dc240c6a9823ed416c923d4901088b10ede6eb72
Janelle-Regina Deseo <168034957+janelledeseo@users.noreply.github.com>
2025-02-20 09:46:57 -0800

    Update reverse.py
    add janelle's function

--- bioinformatics/reverse.py
+++ bioinformatics/reverse.py
@@ -1,1 +1,6 @@
-##### removed reverse fucntion
+### Janelle's Reverse Complement Tool
+
+def rev_comp(string):
+  complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+  reverse_complement = ''.join([complement[base] for base in string[::-1]])
+  return reverse_complement
