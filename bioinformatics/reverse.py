1ff5e4d696f89218ff24c7b17a09a9767ecddff8
Janelle-Regina Deseo <168034957+janelledeseo@users.noreply.github.com>
2025-02-20 10:31:10 -0800

    Update reverse.py
    add line to skip unknown bases

--- bioinformatics/reverse.py
+++ bioinformatics/reverse.py
@@ -1,9 +1,9 @@
 ### Janelle's Reverse Complement Tool
 ## add 'strip' to remove trailing spaces
+## skip unknown bases
 
 def rev_comp(string):
-  complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
-  string = string.strip()
-  reverse_complement = ''.join([complement[base] for base in string[::-1]])
-  return reverse_complement
-
+    complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+    string = string.strip()
+    reverse_complement = ''.join([complement.get(base, '') for base in string[::-1]])
+    return reverse_complement
