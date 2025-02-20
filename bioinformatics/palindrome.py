2abdbe49c9df83ff0b8937d4164b8e6bfae8493c
Joe Uchytil <167820045+Juchytil@users.noreply.github.com>
2025-02-20 11:28:50 -0800

    Palindrome

--- bioinformatics/palindrome.py
+++ bioinformatics/palindrome.py
@@ -1,6 +1,8 @@
 def reverse_palindrome(seq,length):
+  palindromes = []
   rev_seq = seq[::-1]
   n = length
   for i in range(len(seq)):
     if seq[i:i+n] == rev_seq[i:i+n] and len(seq[i:i+n]) == n:
-      return i+1,seq[i:i+n]
+      palindromes.append(seq[i:i+n])
+  return palindromes
