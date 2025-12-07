cf36de8896c729ef742ed17f7758296387a2fe45
J. Cesar Ignacio Espinoza <53498016+phagenomics@users.noreply.github.com>
2025-12-07 13:05:08 -0800

    Update gc_content.py

--- bioinformatics/gc_content.py
+++ bioinformatics/gc_content.py
@@ -1,12 +1,2 @@
-#Cleaning 2 Bugs 
 def gc_content(sequence1):
-    gcontent = sequence1.strip().count('G')
-    ccontent = sequence1.strip().count('C')
-    acontent = sequence1.strip().count('A')
-    tcontent = sequence1.strip().count('T')
-    gc_content = (gcontent + ccontent)/(gcontent + ccontent + acontent + tcontent)
     return gc_content
-
-sequence1 = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
-
-gc_content(sequence1)
