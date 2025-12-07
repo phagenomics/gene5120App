8d5f5c1f9724e77dcd55822e37bc88e8b5124a54
J. Cesar Ignacio Espinoza <53498016+phagenomics@users.noreply.github.com>
2025-12-07 13:14:13 -0800

    Update gc_content.py

--- bioinformatics/gc_content.py
+++ bioinformatics/gc_content.py
@@ -1,2 +1,6 @@
-def gc_content(sequence1):
+def gc_content(sequence):
+    ct_g = sequence.count('G')
+    ct_c = sequence.count('C')
+    length = len(sequence)
+    gc_content = (ct_g + ct_c)/length
     return gc_content
