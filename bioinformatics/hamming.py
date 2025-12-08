aadc96b8b4458d97e1f66b3810236e5fd3068e27
KatAguir <KAGUIRRE25@kgi.edu>
2025-12-08 11:08:17 -0800

    Update hamming.py
    I updated the Hamming function!

--- bioinformatics/hamming.py
+++ bioinformatics/hamming.py
@@ -1,2 +1,8 @@
 def hamming(s,t): #takes in two strings
-    return TRUE
+    if len(s) != len(t):
+        raise ValueError
+    mutations = 0
+    for i in range(len(seq1)):
+     if seq1[i] != seq2[i]:
+      mutations += 1
+    return True
