2baf81bee810c8ef53077ff9aba330e8a03ec12c
J. Cesar Ignacio Espinoza <53498016+phagenomics@users.noreply.github.com>
2025-12-08 14:42:12 -0500

    Update RestrictionEnzymeCutter.py

--- bioinformatics/RestrictionEnzymeCutter.py
+++ bioinformatics/RestrictionEnzymeCutter.py
@@ -1,37 +1,3 @@
 def restriction(seq, enzyme):
     return TRUE
 
-def restriction(seq, enzyme):
-    seq = seq.upper()
-    site = enzyme['site'].upper()
-    cut_offset = enzyme['cut_offset']
-
-    cut_positions = []
-    start = 0
-
-     # Find all occurrences of recognition site
-    while True:
-        pos = seq.find(site, start)
-        if pos == -1:
-            break
-        cut_positions.append(pos + cut_offset)
-        start = pos + 1
-
-    if not cut_positions:
-        return [seq] 
-    # No cuts
-    fragments = []
-    previous = 0
-    for cut in cut_positions:
-        fragments.append(seq[previous:cut])
-        previous = cut
-    fragments.append(seq[previous:])
-    return fragments
-
-if __name__ == "__main__":
- EcoR1 = {
-    "site":"GAATTC",
-    "CUT_OFFSET":1
-}
-dna = "AAGGAATTCCGTTGAATTCGGA"
-print(restriction(dna, EcoR1))
