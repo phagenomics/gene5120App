ebe86214769d738e43bfa969b4884a1636f1729b
emilypasion <epasion25@kgi.edu>
2025-12-08 11:30:39 -0800

    Update reverse.py
    update

--- bioinformatics/reverse.py
+++ bioinformatics/reverse.py
@@ -1,9 +1,7 @@
  def reverse_complement(dna_seq):
-        bp_dict = {'A':'T', 'T':'A', 'C':'G', 'G':'C'}
-        rev = dna_seq[::-1]
-        comp = ''
-        for bp in rev:
-            comp += bp_dict[bp]
-        return comp
-
-reverse_complement(dna_seq)
+    bp_dict = {'A':'T', 'T':'A', 'C':'G', 'G':'C'}
+    rev = dna_seq[::-1]
+    comp = ''
+    for bp in rev:
+        comp += bp_dict[bp]
+    return comp
