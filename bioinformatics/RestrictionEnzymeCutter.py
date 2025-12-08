787d6d384e680a118293ef01d71fbd8a1a745325
kramaesther-maker <kramaesther@gmail.com>
2025-12-08 11:21:33 -0800

    Update RestrictionEnzymeCutter.py

--- bioinformatics/RestrictionEnzymeCutter.py
+++ bioinformatics/RestrictionEnzymeCutter.py
@@ -14,3 +14,23 @@ def restriction(seq, enzyme):
 
     cut_positions = []
     start = 0
+
+      # Find all occurrences of recognition site
+    while True:
+        pos = seq.find(site, start)
+        if pos == -1:
+            break
+        cut_positions.append(pos + cut_offset)
+        start = pos + 1
+
+    if not cut_positions:
+        return [seq] 
+    # No cuts
+      fragments = []
+    previous = 0
+    for cut in cut_positions:
+        fragments.append(seq[previous:cut])
+        previous = cut
+    fragments.append(seq[previous:])
+
+    return fragments
