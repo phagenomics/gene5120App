c645da42727da6e6c6df27e118e1b829ef460c1e
kramaesther-maker <kramaesther@gmail.com>
2025-12-08 11:16:41 -0800

    Update RestrictionEnzymeCutter.py

--- bioinformatics/RestrictionEnzymeCutter.py
+++ bioinformatics/RestrictionEnzymeCutter.py
@@ -1,2 +1,8 @@
 def restriction(seq, enzyme):
+    seq = seq.upper()
+    site = enzyme['site'].upper()
+    cut_pos = enzyme['cut_pos'0]
+    cut_pos1 = enzyme.get("cut_pos1", none)
+    if cut_pos1 is None:
+        cut_pos1 = cut_pos
     return TRUE
