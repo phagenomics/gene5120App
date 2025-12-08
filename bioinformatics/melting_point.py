54fe84a5bb5471ca0ea043bf27ab4c3798b24d53
neorashifrin-kgi <neorashifrin@gmail.com>
2025-12-08 11:37:54 -0800

    Update melting_point.py

--- bioinformatics/melting_point.py
+++ bioinformatics/melting_point.py
@@ -1,6 +1,7 @@
 def melting_temp(sequence):
-    a_t_count = sequence.count ({"A"}) + sequence.count ("T")
-    g_c_count = sequence.count ('G') + sequence.count ('T'
+    a_t_count = sequence.count ("A") + sequence.count ("T")
+    g_c_count = sequence.count ('G') + sequence.count ('T')
     melt = 4 * (g_c_count) + 2 * (a_t_count)
-    return melt
+    return melting_temp
+melting_temp(sequence)
 
