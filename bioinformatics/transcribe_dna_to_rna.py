a04448dfb166fd575b01577ca32ecf34d5adaa0f
J. Cesar Ignacio Espinoza <53498016+phagenomics@users.noreply.github.com>
2026-02-11 20:03:43 -0500

    Update transcribe_dna_to_rna.py

--- bioinformatics/transcribe_dna_to_rna.py
+++ bioinformatics/transcribe_dna_to_rna.py
@@ -1,5 +1,2 @@
 def transcription(dna_string):
-    #replace all Thymine (T) nucleotide bases with Uracil (U)
-    rna_string = dna_string.upper().replace("T", "U")
-    print(rna_string)
     return TRUE
