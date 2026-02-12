40afa9796e87ec64218dc146afd1cc56eca09a82
J. Cesar Ignacio Espinoza <53498016+phagenomics@users.noreply.github.com>
2026-02-11 20:02:44 -0500

    Update reverse_palindrome_seq.py

--- bioinformatics/reverse_palindrome_seq.py
+++ bioinformatics/reverse_palindrome_seq.py
@@ -1,15 +1,4 @@
 def reverse_palindrome(seq,length):
-  results = []
-  n= len(seq)
-
-  def reverse_compliment(seq):
-        complement_map = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
-        complement_seq = "".join([complement_map[base] for base in seq])
-        return complement_seq[::-1]
-
-  for i in range(n):
-        for j in range(i), min(i + 1, n + 1):
-            substring = seq[i:j]
-            return substring
+  return TRUE
 
 #reverse_palindrome(Rosalind_4746, 4, 12)
