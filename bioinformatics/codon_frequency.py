080c2f8c0ba852a95b82f33e5affffb85574c054
neda-sh2 <nshirzadi02@yahoo.com>
2026-02-12 11:25:00 -0800

    Update codon_frequency.py

--- bioinformatics/codon_frequency.py
+++ bioinformatics/codon_frequency.py
@@ -1,10 +1,10 @@
 
+def codon_frequency(input_sequence)
 #Given a DNA or RNA sequence, this program will count the number of codons present (set of 3 letters)
 #Test DNA Sequence: "ATGCGCTATCAGCATGCGCGCGCGAGAGAGA"
-dna_sequence = 
-dna_sequence = dna_sequence.replace(" ", "").replace(",", "")
+input_sequence = input_sequence.replace(" ", "").replace(",", "")
 allowed_nucleotides = set('atcguATCGU') # Allow both uppercase and lowercase
-if not all(char in allowed_nucleotides for char in dna_sequence):
+if not all(char in allowed_nucleotides for char in input_sequence):
     raise ValueError("abnormal nucleotide input")
-num_codons = len(dna_sequence) // 3
+num_codons = len(input_sequence) // 3
 print(f"The number of codons in the sequence is: {num_codons}")
