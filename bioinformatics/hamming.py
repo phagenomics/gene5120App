9fe2098a7ebd6b05deb63eb3046df727ad2def46
J. Cesar Ignacio Espinoza <53498016+phagenomics@users.noreply.github.com>
2026-02-11 20:00:44 -0500

    Update hamming.py

--- bioinformatics/hamming.py
+++ bioinformatics/hamming.py
@@ -1,8 +1,2 @@
 def hamming(s,t): #takes in two strings
-    if len(s) != len(t):
-        raise ValueError
-    mutations = 0
-    for i in range(len(seq1)):
-     if seq1[i] != seq2[i]:
-      mutations += 1
-    return mutations
+    return TRUE
