3f49790d6278398212234dc50c08b4e36dfdcdf0
RaiyaKimble <rkimble24@kgi.edu>
2026-02-12 10:55:00 -0800

    Update reverse.py

--- bioinformatics/reverse.py
+++ bioinformatics/reverse.py
@@ -1,3 +1,4 @@
+## This will be a quick
 def reverse_complement(dna_seq):
     complement = {
         'A': 'T',
@@ -9,9 +10,9 @@ def reverse_complement(dna_seq):
         'c': 'g',
         'g': 'c'
     }
-    
+## Will take the sequence given, match it to the reverse base and join the bases together to form the sequence
     rev_comp = ''.join(complement.get(base, base) for base in reversed(dna_seq))
     
     return rev_comp
-
+## This will print out the reverse complement sequence
 print(reverse_complement(sequence))
