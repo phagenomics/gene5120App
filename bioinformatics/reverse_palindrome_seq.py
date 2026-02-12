8ed7cd4fe6d4584460f572fa708bc90b615eed75
J-Anth0ny <janegret21@gmail.com>
2026-02-12 10:23:38 -0800

    Update reverse_palindrome_seq.py
    This function finds sequence palindromes in a given DNA sequence. An example of how to use the code is provided at the bottom.

--- bioinformatics/reverse_palindrome_seq.py
+++ bioinformatics/reverse_palindrome_seq.py
@@ -1,4 +1,26 @@
-def reverse_palindrome(seq,length):
-  return TRUE
+#Sequence palindrome finder
+def reverse_complement(seq):
+    complement = {'A':'T','T':'A','C':'G','G':'C'}
+    seq = seq.upper()
+    return "".join(complement[base] for base in reversed(seq))
+
+def find_palindromes_window(sequence, min_len=4, max_len=12):
+    sequence = sequence.upper()
+    results = []
+
+    for length in range(min_len, max_len + 1):
+        for i in range(len(sequence) - length + 1):
+            subseq = sequence[i:i+length]
+            rev_comp = reverse_complement(subseq)
+            if subseq == rev_comp:
+                results.append((i, i+length, subseq))
+
+    return results
+
+#Example
+#seq = "ATGCCGATGCGTGTGTCGCGCAATA"
+#palindromes = find_palindromes_window(seq, min_len=4, max_len=12)
+#for _, _, pal in palindromes:
+#    print(pal)
 
 #Upload to GitHub
