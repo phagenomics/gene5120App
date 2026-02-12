dcfeebac53a3362e0a1a88492f04ba47bf3255d7
RaiyaKimble <rkimble24@kgi.edu>
2026-02-12 09:47:43 -0800

    Update reverse.py

--- bioinformatics/reverse.py
+++ bioinformatics/reverse.py
@@ -1,2 +1,15 @@
-def rev_comp(dna_seq):
-    return TRUE
+def reverse_complement(dna_seq):
+    complement = {
+        'A': 'T',
+        'T': 'A',
+        'C': 'G',
+        'G': 'C',
+        'a': 't',
+        't': 'a',
+        'c': 'g',
+        'g': 'c'
+    }
+    
+    rev_comp = ''.join(complement.get(base, base) for base in reversed(dna_seq))
+    
+    return rev_comp
