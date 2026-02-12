11fb2a88cb589f28e45c2e87788bcdaafe1d553d
J. Cesar Ignacio Espinoza <53498016+phagenomics@users.noreply.github.com>
2026-02-11 20:01:34 -0500

    Update melting_point.py

--- bioinformatics/melting_point.py
+++ bioinformatics/melting_point.py
@@ -1,7 +1,3 @@
 def melting_temp(sequence):
-    a_t_count = sequence.count ("A") + sequence.count ("T")
-    g_c_count = sequence.count ('G') + sequence.count ('T')
-    melt = 4 * (g_c_count) + 2 * (a_t_count)
-    return melting_temp
-
+    return TRUE
 
