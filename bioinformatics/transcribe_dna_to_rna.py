ed3f8659891adfe5558d0358392803b4a28d5337
Jensn18 <jjensen24@kgi.edu>
2026-02-12 11:23:23 -0800

    Update transcribe_dna_to_rna.py

--- bioinformatics/transcribe_dna_to_rna.py
+++ bioinformatics/transcribe_dna_to_rna.py
@@ -4,7 +4,7 @@ def transcription(dna_string):
     valid_bases = {'A', 'T', 'G', 'C'}
     if not all(base in valid_bases for base in dna_string):
         invalid_chars = set(char for char in dna_string if char not in valid_bases)
-        raise ValueError(f"Invalid character(s) found in DNA sequence: {', '.join(invalid_chars)}. Only A, T, G, C are allowed.")
+        return ValueError(f"Invalid character(s) found in DNA sequence: {', '.join(invalid_chars)}. Only A, T, G, C are allowed.")
 
     rna_sequence = dna_string.replace('A', 'U')
     return rna_sequence 
