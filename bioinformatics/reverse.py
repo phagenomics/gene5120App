f43745004d200eff5431921ad59dabc1b6ee3602
J. Cesar Ignacio Espinoza <53498016+phagenomics@users.noreply.github.com>
2026-02-11 20:07:12 -0500

    Update reverse.py

--- bioinformatics/reverse.py
+++ bioinformatics/reverse.py
@@ -1,7 +1,2 @@
 def rev_comp(dna_seq):
-    bp_dict = {'A':'T', 'T':'A', 'C':'G', 'G':'C'}
-    rev = dna_seq[::-1]
-    comp = ''
-    for bp in rev:
-        comp += bp_dict[bp]
-    return comp
+    return TRUE
