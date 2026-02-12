fd37fa609fd9ff584a1832848d9fb0a30513cca4
neda-sh2 <nshirzadi02@yahoo.com>
2026-02-12 10:00:08 -0800

    Update codon_frequency.py

--- bioinformatics/codon_frequency.py
+++ bioinformatics/codon_frequency.py
@@ -2,6 +2,6 @@
 #    return TRUE
 #given an DNA or RNA sequence count the number of codons (set of 3 letters)
 
-dna_sequence = "ATGCGCTATCAGCAGAGAGAGA"
+dna_sequence = 
 num_codons = len(dna_sequence) // 3
 print(f"The number of codons in the sequence is: {num_codons}")
